3b9b6ef30662b3bf7eabea905c3e2660498177ed
Mark Lagatuz <mlagatuz@mbp.lan>
2022-10-24 20:42:24 -0700

    comment out transform_csv()

--- paypal_to_cointracker.py
+++ paypal_to_cointracker.py
@@ -5,7 +5,7 @@ class Transactions_Integration:
     def __init__(self, transaction_table = None) -> None:
         self.transaction_table = transaction_table
 
-    def transform_csv(self) -> None:
+    #def transform_csv(self) -> None:
         #self.format_headers()
         #self.format_time()
 
